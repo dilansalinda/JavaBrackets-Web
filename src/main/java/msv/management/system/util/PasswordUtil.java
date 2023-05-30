bc6774d3c224ee7cbe80a0bdfb536bc68d62ac97
dilansalinda <dilan2012salinda@gmail.com>
2023-05-30 16:35:45 +0530

    #8 issue fixed

--- src/main/java/msv/management/system/util/PasswordUtil.java
+++ src/main/java/msv/management/system/util/PasswordUtil.java
@@ -50,23 +50,18 @@ public class PasswordUtil {
     public static String decrypt(String encrypted) {
         try {
             if (encrypted != null) {
-                IvParameterSpec iv = new IvParameterSpec(initVector.getBytes(Charset.defaultCharset()));
-                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(Charset.defaultCharset()), "AES");
+                IvParameterSpec iv = new IvParameterSpec(initVector.getBytes(StandardCharsets.UTF_8));
+                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
 
-                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
                 cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
                 byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
 
                 return new String(original, StandardCharsets.UTF_8);
             }
-        } catch (
-                NoSuchPaddingException |
-                NoSuchAlgorithmException |
-                BadPaddingException |
-                InvalidAlgorithmParameterException |
-                InvalidKeyException |
-                IllegalBlockSizeException ex) {
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException | BadPaddingException
+                 | InvalidAlgorithmParameterException | InvalidKeyException | IllegalBlockSizeException ex) {
             LOGGER.error("Error while decrypting password! ", ex);
         }
         return encrypted;
